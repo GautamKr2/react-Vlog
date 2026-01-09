6f80448988e9b70e3d2da01816e809e71e11ef38
GautamKr2 <gautamkumar11008@gmail.com>
2026-01-10 01:02:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from './Header'
+
+function App() {
+  return <>
+    <h1> Nahi chal raha hai kya? </h1>
+    <h1> Hello guys </h1>
+    <Header />
+  </>
+}
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+    return <>
+        <h2> Hello react </h2>
+        <h2>I'm starting reart.</h2>
+    </>
+}
+export default Header
